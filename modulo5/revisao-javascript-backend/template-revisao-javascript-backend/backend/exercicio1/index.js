f1dcac36f67036f41cb71ed309396849ea29cfd6
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2022-10-04 20:40:53 -0300

    inicio da revisão JS - Pré Backend

---
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio1/index.js
@@ -0,0 +1,23 @@
+//Parte 1 - Revisão JS para o Back-end 
+
+let client = {}
+
+client = {
+  user: "Leandro",
+  conta: 1234567,
+  age: 27,
+  login: () =>{
+    console.log(`Usuário logado com sucesso! Bem vindo ${client.user} :)`);
+  }
+}
+
+client.login()
+
+function sum(a, b) {
+  return a + b
+  }
+  console.log(sum(10, 6))
+
+//Exercício parte 1
+
+
