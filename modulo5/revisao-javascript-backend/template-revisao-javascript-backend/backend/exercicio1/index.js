842e2d4ef95314927a074be3cd6a8ac3f485371b
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2022-10-05 21:19:42 -0300

    seguindo com os exercícios da revisão

--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,23 +1,39 @@
-//Parte 1 - Revisão JS para o Back-end 
+//Parte 1 - Revisão JS para o Back-end
 
-let client = {}
+// Exercícios de exemplo
+let client = {};
 
 client = {
   user: "Leandro",
-  conta: 1234567,
+  account: 1234567,
   age: 27,
-  login: () =>{
+  login: () => {
     console.log(`Usuário logado com sucesso! Bem vindo ${client.user} :)`);
-  }
-}
+  },
+};
 
-client.login()
+client.login();
 
 function sum(a, b) {
-  return a + b
+  return a + b;
+}
+console.log(sum(10, 6));
+
+//Exercício Parte 1
+
+const validation = (item) => {
+  console.log(item);
+};
+
+function temperatureConverter(item, number, callback) {
+  if (item === "K") {
+    callback(` ${number}° Celsius é equivalente a ${number + 273.15}° Kelvin`);
+  } else if (item === "F") {
+    callback(
+      `  ${number}° Celsius é equivalente a ${1.8 * number + 32}° Farenheit`
+    );
+  } else if (item !== "F" || item !== "K") {
+    callback(`Erro. Parâmentro ${item} inválido`);
   }
-  console.log(sum(10, 6))
-
-//Exercício parte 1
-
-
+}
+temperatureConverter("K", "30", validation);
--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,1 +1,26 @@
-console.log("exercício 2");
+// Exercício Parte 2
+
+let clients = [
+  { id: 1, nome: "Fulano" },
+  { id: 2, nome: "Ciclano" },
+  { id: 3, nome: "Beltrano" },
+  { id: 4, nome: "Fulana" },
+];
+
+newClient = { id: 5, nome: "Leandro" };
+console.log(clients);
+//Validação clientes no array
+
+if (newClient.id === clients.id) {
+  console.log(`Erro. Parâmetro inválido, o  "id" já existe`);
+} else {
+  clients.push(newClient);
+}
+
+// Tabuada 
+
+let tabledNumber = (5)
+for(let i = 0; i <= 10; i++){
+  console.log(`${i} x ${tabledNumber} = ${i*tabledNumber}`);
+
+}
