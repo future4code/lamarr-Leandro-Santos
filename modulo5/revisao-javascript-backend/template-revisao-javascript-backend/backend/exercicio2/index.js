0bad025b7acc39409a7a7f4d1df781d644babe77
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2022-10-06 21:46:35 -0300

    seguindo com revisão

--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio2/index.js
@@ -7,20 +7,26 @@ let clients = [
   { id: 4, nome: "Fulana" },
 ];
 
-newClient = { id: 5, nome: "Leandro" };
+newClient = { id: 3, nome: "Leandro" };
 console.log(clients);
 //Validação clientes no array
 
-if (newClient.id === clients.id) {
+if (newClient.id === [clients.id]) {
   console.log(`Erro. Parâmetro inválido, o  "id" já existe`);
 } else {
   clients.push(newClient);
 }
+console.log(clients);
 
-// Tabuada 
+// Tabuada e validação
+tabledNumber = "5";
 
-let tabledNumber = (5)
-for(let i = 0; i <= 10; i++){
-  console.log(`${i} x ${tabledNumber} = ${i*tabledNumber}`);
-
+if (tabledNumber === String(tabledNumber))
+  console.log(`Erro. Parâmetro inválido (deve ser digitado um número).`);
+else if (tabledNumber > 10)
+  console.log("Erro. Parâmetro inválido (número precisa valer entre 1 e 10).");
+else {
+  for (let i = 0; i <= 10; i++) {
+    console.log(`${i} x ${tabledNumber} = ${i * tabledNumber}`);
+  }
 }
--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio3/index.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio3/index.js
@@ -1,1 +1,14 @@
-console.log("exercicio 3");
+// Exercício Parte 3
+console.log("Exercício Parte 3");
+
+const accounts = [
+	{
+		email: "astrodev@labenu.com",
+		password: "abc123"
+	},
+	{
+		email: "bananinha@gmail.com",
+		password: "bananinha"
+	}
+]
+
