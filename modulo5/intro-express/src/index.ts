2b08059710464cb727e2887ca944f896f7a18060
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2022-10-27 20:19:24 -0300

    end :white_check_mark:

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -58,9 +58,9 @@ const usersList: User[] = [
 // Exercício 04
 
 app.get("/users", (req: Request, res: Response) => {
-let users = usersList.map((user)=>{
-  return user 
-})
+  let users = usersList.map((user) => {
+    return user;
+  });
 
   res.status(200).send(users);
 });
@@ -71,52 +71,62 @@ type Post = {
   id: number;
   title: string;
   body: string;
-  userId: number;  
+  userId: number;
 };
 
- // Exercício 06
- // Prefiro criar dentro de um array, pois acho que fica mais organizado e padrão com as apis já vistas
+// Exercício 06
+// Prefiro criar dentro de um array, pois acho que fica mais organizado e padrão com as apis já vistas
 
- const postList: Post[] = [
+const postList: Post[] = [
   {
     id: 1,
     userId: 1,
     title: "Como fazer API",
-    body: "Como fazer uma api bacana para o site com todos os recursos necessários",  
-    
+    body: "Como fazer uma api bacana para o site com todos os recursos necessários",
   },
   {
     id: 2,
     userId: 2,
     title: "Como encontrar a solução para bugs",
-    body: "Como encontrar a solução para bugs debugando o código e o deixando mais clean",  
-    
+    body: "Como encontrar a solução para bugs debugando o código e o deixando mais clean",
   },
   {
     id: 3,
     userId: 3,
     title: "Tudo o que você precisa saber sobre Express",
-    body: "udo o que você precisa saber sobre Express, guia inicial passo a passo",  
-    
+    body: "udo o que você precisa saber sobre Express, guia inicial passo a passo",
   },
   {
     id: 4,
     userId: 4,
     title: "O que fazer para conquistar uma vaga",
-    body: "O que fazer para conquistar uma vaga, trazemos as principais dicas para conseguir a primeira vaga dev",  
-    
-  },  
-  
+    body: "O que fazer para conquistar uma vaga, trazemos as principais dicas para conseguir a primeira vaga dev",
+  },
 ];
 //Exercício 07
 
 app.get("/posts", (req: Request, res: Response) => {
-  let users = postList.map((post)=>{
-    return post 
-  })
-  
-    res.status(200).send(users);
+  let users = postList.map((post) => {
+    return post;
   });
+
+  res.status(200).send(users);
+});
+
+//Exercício 08
+
+app.get("/posts", (req: Request, res: Response) => {
+  const postId = Number(req.query.userId);
+  if (!postId) {
+    res.status(400).send("Colocar obrigatoriamente um id");
+  }
+
+  const selectedPost = postList.filter((posts) => {
+    return posts.userId === postId;
+  });
+
+  res.status(200).send(selectedPost);
+});
 
 
 
