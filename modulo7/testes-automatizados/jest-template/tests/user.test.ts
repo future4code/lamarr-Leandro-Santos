851d1120443e211d75ccd971ff6d7ca21b252a6a
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2023-02-28 20:22:49 -0300

    exercicio jest

---
+++ modulo7/testes-automatizados/jest-template/tests/user.test.ts
@@ -0,0 +1,43 @@
+
+import { User } from "../src/model/User";
+import { performPurchase } from "../src/Users";
+
+
+test("Testing balance greater than value", () => {
+  const user: User = {
+    name: "Astrodev",
+    balance: 100,
+  };
+
+  const result = performPurchase(user, 40);
+
+  expect(result).toEqual({
+    name: "Astrodev",
+    balance: 60,
+  });
+
+  test("Testing balance greater than value", () => {
+    const user: User = {
+      name: "Astrodev",
+      balance: 50
+    }
+  
+    const result = performPurchase(user, 50)
+    
+    expect(result).toEqual({
+      ...user,
+      balance: 0
+    })
+  })
+
+  test("Testing balance greater than value", () => {
+    const user: User = {
+      name: "Astrodev",
+      balance: 30
+    }
+  
+    const result = performPurchase(user, 50)
+    
+    expect(result).not.toBeDefined()
+  })
+});
